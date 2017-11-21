80d02681098163259e08026b13baa0560490036c
Alexandre Swioklo <alexswi@hotmail.com>
2017-11-21 00:24:54 -0200

    Added Ticker.Once

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266HTTPUpdateServer.h>
+#include <Ticker.h>
 #include "credentials.h"
 
 const char* hostName = "GaragemX";
@@ -14,16 +15,20 @@ const char* password = PASSWORD;
 
 IPAddress ip(10,1,1,52);  
 
-int status1 = 0;
-int status2 = 0;
-
-
+Ticker tickerSetOff;
 
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
 
+
 void handleRoot() {
   httpServer.send(200, "text/plain", "hello from esp8266!");
+}
+
+void relayOff(uint8_t pin){
+  digitalWrite(pin,0); 
+  Serial.print(pin);
+  Serial.println(" Off");
 }
 
 void handleNotFound(){
@@ -43,13 +48,12 @@ void handleNotFound(){
 }
 
 
+
+
 void setup(void){
   pinMode(D1, OUTPUT); 
   pinMode(D2, OUTPUT); 
   Serial.begin(115200);
-//  delay(200);
-//  Serial.println();
-//  Serial.println("Booting Sketch...");
   IPAddress gateway(10, 1,1, 1);  
   IPAddress subnet(255, 255, 255, 0);  
   WiFi.config(ip, gateway, subnet);
@@ -63,30 +67,27 @@ void setup(void){
 		delay ( 500 );
 		Serial.print ( "." );
 	}
- 
-  
+
+  Serial.println("");
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
 
   httpUpdater.setup(&httpServer, update_path, update_username, update_password);
     httpServer.on("/", handleRoot);
 
   httpServer.on("/inline1", [](){
-    status1 = !status1;
-    digitalWrite(D1, status1); 
-    Serial.print("Status 1: ");
-    Serial.println(status1);
-    httpServer.send(200, "text/plain", "this works as well");
+    digitalWrite(D1, 1); 
+    Serial.print("D1 On");
+    tickerSetOff.once_ms(300, relayOff, D1);
+    httpServer.send(200, "text/plain", "D1 On");
   });
 
   httpServer.on("/inline2", [](){
-    status2 = !status2;
-    digitalWrite(D2, status2); 
-    Serial.print("Status 2: ");
-    Serial.println(status2);
-    httpServer.send(200, "text/plain", "this works as well");
+    digitalWrite(D2, 1); 
+    Serial.println("D2 On");
+    tickerSetOff.once_ms(300, relayOff, D2);
+    httpServer.send(200, "text/plain", "D2 On");
   });
   
 
